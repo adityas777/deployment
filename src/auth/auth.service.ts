8958a70cec86da4789e261204a43b6ca32da9cdf
ADITYA SINGH <pglapgla123@gmail.com>
2025-07-01 16:24:13 +0530

    fix: use bcryptjs instead of bcrypt for compatibility

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable, UnauthorizedException, BadRequestException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
